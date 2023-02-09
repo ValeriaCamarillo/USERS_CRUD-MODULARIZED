cdbdfd6587a3b4fea63d4f368798d2e9e536e2ba
valeriacamarillo <valeria1213@gmail.com>
2023-02-08 19:53:21 -0500

    first commit

---
+++ flask_app/models/user.py
@@ -0,0 +1,57 @@
+# import the function that will return an instance of a connection
+from flask_app.config.mysqlconnection import connectToMySQL
+
+# model the class after the user table from our database
+
+DATABASE = "users_cr"
+
+class User:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    # Now we use class methods to query our database
+
+
+    ## ! CREATE
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s, %(email)s);"
+        return connectToMySQL(DATABASE).query_db(query, data)
+
+
+    ## ! READ/RETRIEVE ALL
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL(DATABASE).query_db(query)
+        users = []
+        for user in results:
+            users.append( User(user) )
+        return users
+
+
+    # ! READ/RETRIEVE ONE
+    @classmethod
+    def get_one(cls,data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL(DATABASE).query_db(query, data)
+        user = User(result[0])
+        return user
+    
+
+      # ! UPDATE
+    @classmethod
+    def update(cls,data):
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s ;"
+        return connectToMySQL(DATABASE).query_db(query, data)
+
+
+    # ! DELETE
+    @classmethod
+    def destroy(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL(DATABASE).query_db(query, data)
